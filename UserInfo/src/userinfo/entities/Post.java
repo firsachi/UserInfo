047b5f99d61f607014b44f005d860d67bb8f1887
firsov <firsov@firsbook>
2015-12-02 21:21:13 +0200

    objek post

---
+++ UserInfo/src/userinfo/entities/Post.java
@@ -0,0 +1,48 @@
+package userinfo.entities;
+
+/**
+ * Created by firsov on 24.09.2015.
+ */
+public class Post implements RowEntities{
+
+    private int postID;
+    private String postName;
+    private boolean postUnique;
+    private boolean postBusy;
+
+    public Post(){
+        this.postID=0;
+    }
+
+    public void setPostID(int postID){
+        this.postID=postID;
+    }
+    public  void setPostName(String postName){
+        this.postName=postName;
+    }
+    public  void  setPostUnique(boolean postUnique){
+        this.postUnique=postUnique;
+    }
+    public void setPostBusy(boolean postBusy){
+        this.postBusy=postBusy;
+    }
+
+    public int getPostID(){
+        return postID;
+    }
+    public String getPostName(){
+        return postName;
+    }
+    public boolean getPostUnique(){
+        return  postUnique;
+    }
+    public  boolean getPostBusy(){
+        return postBusy;
+    }
+
+    @Override
+    public String[] getRow() {
+        String[] row = new String[]{Integer.toString(postID), postName};
+        return row;
+    }
+}
