3ee6ce357cdc15503b5d4cc9db2ad9ed06a6bf16
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-03 15:57:09 +0200

    Interface for settings user info

---
+++ UserInfo/src/userinfo/gui/frameuser/ConfigFrameUserInfo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.frameuser;
+
+/**
+ *
+ * @author firsov
+ */
+public interface ConfigFrameUserInfo {
+    
+    String nameButtonAdd();
+    String nameButtonDelete();
+    String nameButtonUpdate();
+    String nameToggleButtonUser();
+    String nameToggleButtonPost();
+    String nameToggleButtonDeaprtment();
+}
