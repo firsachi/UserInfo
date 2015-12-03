f6432581a83d6a4bfab60a43d31a9df3362a830d
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-03 14:16:40 +0200

    table modelfor FameUserInfo

---
+++ UserInfo/src/userinfo/gui/frameuser/tablemodel/TableModel.java
@@ -0,0 +1,37 @@
+package userinfo.gui.frameuser.tablemodel;
+
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+import userinfo.entities.RowEntities;
+
+public class TableModel extends DefaultTableModel {
+
+    public TableModel(){
+        
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column){
+        return false;
+    }
+
+    public void clearTableModel(){
+        TableModel.this.setColumnCount(0);
+        while (TableModel.this.getRowCount() > 0){
+            TableModel.this.removeRow(0);
+        }
+    }
+
+    public void addNameColumn(NameColumn nameColumn){
+        setColumnCount(0);
+        for (String name : nameColumn.getNAmeColumn()){
+            addColumn(name);
+        }
+    }
+
+    public void fillTableModel(ArrayList<RowEntities> arrayList){
+        for (RowEntities rowData : arrayList){
+            addRow(rowData.getRow());
+        }
+    }
+}
