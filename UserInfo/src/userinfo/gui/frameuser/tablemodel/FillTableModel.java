af02854b375bdb360839a24555eff5f1751bf660
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-04 11:41:53 +0200

    Fill table model from in frame FrameUserInfo

--- UserInfo/src/userinfo/gui/frameuser/tablemodel/FillTableModel.java
+++ UserInfo/src/userinfo/gui/frameuser/tablemodel/FillTableModel.java
@@ -5,6 +5,11 @@
  */
 package userinfo.gui.frameuser.tablemodel;
 
+import userinfo.dao.DaoFactory;
+import userinfo.dao.DepartmentDAO;
+import userinfo.dao.PostDAO;
+import userinfo.dao.UsersDAO;
+import userinfo.entities.RowEntities;
 import userinfo.gui.frameuser.EnumNameModel;
 
 /**
@@ -13,20 +18,37 @@ import userinfo.gui.frameuser.EnumNameModel;
  */
 public class FillTableModel {
 
+    private DaoFactory factory;
+    
     public FillTableModel() {
     }
        
-    public void fillTable(TableModel tableModel,EnumNameModel enumNameModel){
+    public void fillTable(TableModel tableModel,EnumNameModel enumNameModel, boolean delete){
         tableModel.clearTableModel();
         switch (enumNameModel){
             case USER:
                 tableModel.addNameColumn(new UserNameColumn());
+                factory = DaoFactory.getInstance(DaoFactory.DaoType.POSTGRESQL);
+                UsersDAO usersDAO = factory.createUsersDAO();
+                for (RowEntities rowData : usersDAO.findAll(delete) ){
+                    tableModel.addRow(rowData.getRow());
+                }
                 break;
             case POST:
                 tableModel.addNameColumn(new PostNameColumn());
+                factory = DaoFactory.getInstance(DaoFactory.DaoType.POSTGRESQL);
+                PostDAO postDAO = factory.createPostDAO();
+                for (RowEntities rowData : postDAO.findAll(delete)){
+                    tableModel.addRow(rowData.getRow());
+                }
                 break;
             case DEPARTMENT:
                 tableModel.addNameColumn(new DepartmentNameColumn());
+                factory = DaoFactory.getInstance(DaoFactory.DaoType.POSTGRESQL);
+                DepartmentDAO departmentDAO = factory.createDepartmentDAO();
+                for (RowEntities rowData : departmentDAO.findAll(delete)){
+                    tableModel.addRow(rowData.getRow());
+                }
                 break;
         }
     }
