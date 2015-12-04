59b9b7d941127624048edf16f7078c7ac179b492
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-04 11:40:07 +0200

    Interface DAO for department

---
+++ UserInfo/src/userinfo/dao/DepartmentDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.dao;
+
+import java.util.ArrayList;
+import userinfo.entities.Department;
+
+/**
+ *
+ * @author firsov
+ */
+public interface DepartmentDAO {
+    
+    Department create(Department department);
+    Department find(int idDepartment);
+    ArrayList<Department> findAll(boolean delete);
+    void delete(int idDepartment);
+    void update(Department department);
+}
