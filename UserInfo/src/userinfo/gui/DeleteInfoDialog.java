394b075cf0ce860ed646b726f63abed1fecb7761
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-11 16:55:45 +0200

    LiAction listener button users

---
+++ UserInfo/src/userinfo/gui/DeleteInfoDialog.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author firsov
+ */
+public class DeleteInfoDialog extends JDialog{
+    
+    private JLabel labelInfo;
+    private JButton buttonOk;
+    private JButton buttonCansel;
+
+    public DeleteInfoDialog() {
+        configDeleteDialog();
+    }
+    
+    private void configDeleteDialog(){
+        setTitle("");
+        
+                
+    }
+}
