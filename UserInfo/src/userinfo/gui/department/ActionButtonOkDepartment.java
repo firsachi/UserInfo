c509ed951e2a24372a37dc5c52a4ae30a300b4ea
firsov <firsov@firsbook>
2015-12-14 17:35:18 +0200

    Action button ok for department dialog

---
+++ UserInfo/src/userinfo/gui/department/ActionButtonOkDepartment.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.department;
+
+import userinfo.gui.DialogDepartment;
+
+/**
+ *
+ * @author firsov
+ */
+public class ActionButtonOkDepartment {
+
+    public ActionButtonOkDepartment() {
+    }
+
+    
+    public void runAction(DialogDepartment dialogDepartment) {
+        switch (dialogDepartment.getAction()){
+            case INSERT:
+                DeaprtmentAdd deaprtmentAdd = new DeaprtmentAdd();
+                deaprtmentAdd.createDepartment(dialogDepartment);
+                break;
+            case UPDATE:
+                break;
+        }
+    }
+       
+}
