bb70ec62d6912af25fbcfb1ea4d8c8240ae1be63
firsov <firsov@firsbook>
2015-12-14 17:38:38 +0200

    Startegy update inteface

---
+++ UserInfo/src/userinfo/gui/staretegyupdate/ButtonUpdateStarategy.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.staretegyupdate;
+
+import userinfo.entities.Department;
+import userinfo.entities.RowEntities;
+
+/**
+ *
+ * @author firsov
+ */
+public interface ButtonUpdateStarategy {
+    
+    RowEntities getUpdateRow(Department department);
+}
