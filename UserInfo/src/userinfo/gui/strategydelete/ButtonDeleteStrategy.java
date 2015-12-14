8a0af6200c84979c3c71a6cc28c14f26fe97e237
firsov <firsov@firsbook>
2015-12-14 17:42:27 +0200

    Interface delete of strategy delete

---
+++ UserInfo/src/userinfo/gui/strategydelete/ButtonDeleteStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.strategydelete;
+
+/**
+ *
+ * @author firsov
+ */
+public interface ButtonDeleteStrategy {
+    
+    boolean deleteRow(int id);
+    
+}
