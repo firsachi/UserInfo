51f0472911d25ba85f8edbad38563387a773c6d3
firsov <firsov@firsbook>
2015-12-20 14:51:17 +0200

    Postgresql post

--- UserInfo/src/userinfo/dao/postgresql/PostgresqlPost.java
+++ UserInfo/src/userinfo/dao/postgresql/PostgresqlPost.java
@@ -8,6 +8,7 @@ package userinfo.dao.postgresql;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,25 +22,52 @@ import userinfo.entities.Post;
  */
 public class PostgresqlPost  implements PostDAO{
 
+    private Post post;
+    
     @Override
     public Post create(Post post) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "INSERT INTO post (name_post,unique_post) VALUES (?,?)";
+        try (PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);) 
+        {
+            preparedStatement.setString(1, post.getPostName());
+            preparedStatement.setBoolean(2, post.getPostUnique());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()){
+                post.setPostID(resultSet.getInt(1));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresqlPost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return post;
     }
 
     @Override
     public Post find(int idPost) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "SELECT * FROM post WHERE id_post=?";
+        try ( PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query);)
+        {
+            preparedStatement.setInt(1, idPost);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                post = getPost(resultSet);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresqlPost.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return post;
     }
 
     @Override
     public ArrayList<Post> findAll(boolean delete) {
-        String query = "SELECT * FROM post WHERE delete_post=?";
+        String query = "SELECT * FROM post WHERE delete_post=? ORDER BY id_post";
         ArrayList<Post> arrayListPost = new ArrayList<>();
         try(PreparedStatement preparedStatement =ReceiveConnect.getConnectionDatabase().prepareStatement(query);) {
             preparedStatement.setBoolean(1, delete);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
-                arrayListPost.add(getPOst(resultSet));
+                arrayListPost.add(getPost(resultSet));
             }
             
         } catch (SQLException ex) {
@@ -49,35 +77,46 @@ public class PostgresqlPost  implements PostDAO{
     }
 
     @Override
-    public void delete(int idPost) {
-        String query = "UPDATE post SET delete_post=true WHERE id_post=?";
-        try(
-                PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query);
-                ) 
+    public boolean delete(int idPost) {
+        boolean resultAction = false;
+        String query = "UPDATE post SET delete_post=true WHERE id_post=? AND 0=(SELECT COUNT(*) FROM users WHERE id_post=?)";
+        try(PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query);) 
         {
             preparedStatement.setInt(1, idPost);
-            preparedStatement.executeQuery();
+            preparedStatement.setInt(2, idPost);
+            if (0 < preparedStatement.executeUpdate()){
+                resultAction = false;
+            }
         } catch (SQLException ex) {
             Logger.getLogger(PostgresqlPost.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return resultAction;
     }
 
     @Override
-    public void update(Post post) {
-        String query = "UPDATE post SET name_post=? WHERE id_post=?";
+    public boolean update(Post post) {
+        boolean result = false;
+        String query = "UPDATE post SET name_post=?,unique_post=? WHERE id_post=? AND delete_post=false AND 0=(SELECT COUNT(*) FROM post WHERE id_post!=? AND name_post=? AND delete_post=false)";
         try (
                 PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query);
                 )
         {
             preparedStatement.setString(1, post.getPostName());
-            preparedStatement.setInt(2, post.getPostID());
-            preparedStatement.executeQuery();
+            preparedStatement.setBoolean(2, post.getPostUnique());
+            preparedStatement.setInt(3, post.getPostID());
+            preparedStatement.setInt(4, post.getPostID());
+            preparedStatement.setString(5, post.getPostName());
+            int res = preparedStatement.executeUpdate();
+            if (0 < res){
+                result = true;
+            }
         } catch (SQLException ex) {
             Logger.getLogger(PostgresqlPost.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return result;
     }
 
-    private Post getPOst(ResultSet resultSet) {
+    private Post getPost(ResultSet resultSet) {
         Post post = new Post();
         try {
             post.setPostID(resultSet.getInt(1));
@@ -89,6 +128,21 @@ public class PostgresqlPost  implements PostDAO{
         }
         
         return post;
+    }
+
+    @Override
+    public boolean findName(String name) {
+        boolean result = false;
+        String query = "SELECT name_post FROM post WHERE name_post=? AND delete_post=false";
+        try(PreparedStatement preparedStatement = ReceiveConnect.getConnectionDatabase().prepareStatement(query);) 
+        {
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            result = resultSet.next();
+        } catch (SQLException ex) {
+            Logger.getLogger(PostgresqlPost.class.getName()).log(Level.SEVERE, null, ex);
+        }       
+        return result;
     }
     
 }
