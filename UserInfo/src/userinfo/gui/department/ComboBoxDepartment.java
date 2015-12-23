7a6dd68316365c154627546f0047666110693888
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-23 18:22:53 +0200

    ComboBox for department objekt

---
+++ UserInfo/src/userinfo/gui/department/ComboBoxDepartment.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.department;
+
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+import userinfo.dao.DaoFactory;
+import userinfo.dao.DepartmentDAO;
+import userinfo.entities.Department;
+
+/**
+ *
+ * @author firsov
+ */
+public class ComboBoxDepartment extends JComboBox<Object>{
+    
+    private final ArrayList<Department> arrayListDeaprtment;
+
+    public ComboBoxDepartment() {
+        DaoFactory daoFactory = DaoFactory.getInstance(DaoFactory.DaoType.POSTGRESQL);
+        DepartmentDAO departmentDAO = daoFactory.createDepartmentDAO();
+        arrayListDeaprtment = departmentDAO.findAll(false);
+        createCombobox();
+    }
+
+    private void createCombobox() {
+        for (Department department : arrayListDeaprtment){
+            addItem(department.getDepartmentName());
+        }
+    }
+    
+    
+}
