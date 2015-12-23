8b1ff38d3903945495c9f5a9050c2c51aaa9693c
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2015-12-23 18:23:13 +0200

    ComboBox for post objekt

---
+++ UserInfo/src/userinfo/gui/post/ComboBoxPost.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinfo.gui.post;
+
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+import userinfo.dao.DaoFactory;
+import userinfo.dao.PostDAO;
+import userinfo.entities.Post;
+
+/**
+ *
+ * @author firsov
+ */
+public class ComboBoxPost extends JComboBox<Object>{
+    
+    private ArrayList<Post> arrayListPost;
+
+    public ComboBoxPost() {
+        DaoFactory daoFactory = DaoFactory.getInstance(DaoFactory.DaoType.POSTGRESQL);
+        PostDAO postDao = daoFactory.createPostDAO();
+        arrayListPost = postDao.findAll(false);
+        fillComboBox();
+    }
+
+    private void fillComboBox() {
+        for (Post post : arrayListPost){
+            addItem(post.getPostName());
+        }
+    }
+    
+    
+}
